70c9111f43850200762d570e9c6d8c1ff2ce493c
MinelaP <minela.piljug.23@size.ba>
2025-11-24 21:35:58 +0100

    Popravke za pokretanje lokanlo baze da vidim da li radi

--- src/main/java/unze/ptf/RouteVisionApp.java
+++ src/main/java/unze/ptf/RouteVisionApp.java
@@ -1,15 +1,17 @@
 package unze.ptf;
 
 import unze.ptf.config.DatabaseConfig;
-
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class RouteVisionApp {
+
     public static void main(String[] args) {
         try (Connection conn = DatabaseConfig.getConnection()) {
             if (conn != null) {
                 System.out.println("Povezano!");
+            } else {
+                System.out.println("Konekcija nije uspjela!");
             }
         } catch (SQLException e) {
             System.out.println("Nema konekcije!");
