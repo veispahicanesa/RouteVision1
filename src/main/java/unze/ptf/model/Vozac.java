8f985193741595c670b305066d0c144f4d6476a0
veispahicanesa <Veispahic@DESKTOP-OSOCAGA>
2025-11-25 18:33:57 +0100

    Dodala sam samo osnovne podatke.

--- src/main/java/unze/ptf/model/Vozac.java
+++ src/main/java/unze/ptf/model/Vozac.java
@@ -1,4 +1,10 @@
 package unze.ptf.model;
-
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 public class Vozac {
+    private int id;
+    private String ime;
+    private String prezime;
+    private String email;
+    private String lozinka;
 }
